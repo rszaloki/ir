6a6abb5c349e43b5df75ae414b5e506964d5e6d2
Robert Szaloki <r.szaloki@gmail.com>
2020-12-13 21:52:25 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 maqueen.IR_callbackUser(function (message) {
     if (maqueen.IR_read() == 10) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
-        basic.pause(800)
+        basic.pause(100)
     }
     if (maqueen.IR_read() == 12) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
-        basic.pause(800)
+        basic.pause(100)
     }
     if (maqueen.IR_read() == 6) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
@@ -26,11 +26,34 @@ maqueen.IR_callbackUser(function (message) {
             maqueen.servoRun(maqueen.Servos.S1, 0)
         }
     }
+    if (maqueen.IR_read() == 3) {
+        stripon = (stripon + 1) % 3
+    }
+    if (maqueen.IR_read() == 95) {
+        basic.showString("menu")
+    }
     maqueen.motorStop(maqueen.Motors.All)
+    basic.showString("" + (stripon))
+    if (stripon == 0) {
+    	
+    }
+    if (stripon == 1) {
+        strip.setBrightness(255)
+        strip.showRainbow(1, 360)
+        strip.show()
+    }
 })
+let stripon = 0
+let strip: neopixel.Strip = null
 let lapat = 0
 lapat = 0
 maqueen.servoRun(maqueen.Servos.S1, 0)
+strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
+stripon = 0
 basic.forever(function () {
-	
+    if (stripon == 2) {
+        strip.rotate(1)
+        strip.show()
+        basic.pause(100)
+    }
 })
